cb84770152b192abbc5478f804387dbf3b8e08a5
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2024-02-23 11:33:02 +0100

    Hacking

--- src/test/java/com/example/assertj/conversion/CustomAssertTests.java
+++ src/test/java/com/example/assertj/conversion/CustomAssertTests.java
@@ -1,11 +1,15 @@
 package com.example.assertj.conversion;
 
+import java.util.List;
+
 import org.assertj.core.api.AssertProvider;
+import org.assertj.core.api.Assertions;
 import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.Test;
 
+import org.springframework.core.ParameterizedTypeReference;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
@@ -22,6 +26,22 @@ class CustomAssertTests {
 	@Test
 	void convertToUsingValueType() {
 		assertThat(forValue(123)).convertTo(ValueType.STRING).startsWith("1");
+	}
+
+	@Test
+	void convertToListOfObject() {
+		Customer[] customers = new Customer[] { new Customer("123", "John", "Smith", 42) };
+
+		assertThat(forValue(customers)).convertTo(ValueType.listOf(Customer.class))
+				.hasSize(1).contains(new Customer("123", "John", "Smith", 42));
+	}
+
+	@Test
+	void convertToListOfObjectUsingGenericType() {
+		Customer[] customers = new Customer[] { new Customer("123", "John", "Smith", 42) };
+
+		assertThat(forValue(customers)).convertTo(ValueType.of(new ParameterizedTypeReference<List<Customer>>() {}, value -> Assertions.assertThat((List<Customer>) value)))
+				.hasSize(1).contains(new Customer("123", "John", "Smith", 42));
 	}
 
 
---
+++ src/test/java/com/example/assertj/conversion/Customer.java
@@ -0,0 +1,6 @@
+package com.example.assertj.conversion;
+
+public record Customer(String id, String firstName, String lastName, int age) {
+
+
+}
